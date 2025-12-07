36bef26f88453bb48dcf61617bbc3d3a9a8e4e8b
takeshid <goriponikeike55@gmail.com>
2025-12-07 22:09:43 +0900

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Rust development environment";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     fenix = {
       url = "https://flakehub.com/f/nix-community/fenix/0.1";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -63,6 +63,7 @@
               cargo-watch
               rust-analyzer
               mdbook
+              mdbook-mermaid
             ];
             env = {
               # Required by rust-analyzer
--- step_by_step/02_tracked/src/main.rs
+++ step_by_step/02_tracked/src/main.rs
@@ -16,11 +16,29 @@ struct SourceProgram {
 // Tracked Struct
 #[salsa::tracked(debug)]
 struct Program<'db> {
+    #[tracked]
     lines: Vec<String>,
+}
+
+#[salsa::tracked]
+fn parse_source(db: &dyn salsa::Database, source: SourceProgram) -> Program<'_> {
+    let text = source.text(db);
+    let result: Vec<String> = text.as_str().lines().map(String::from).collect();
+    Program::new(db, result)
 }
 
 fn main() {
     let db = RootDatabase::default();
-    let program = Program::new(&db, vec!["hello".to_string(), "world".to_string()]);
-    println!("{:#?}", program);
+    {
+        let source = SourceProgram::new(&db, "Hello\nWorld\nEveryOne".to_string());
+        let program = parse_source(&db, source);
+        println!("Source: {:#?}", source);
+        println!("Program: {:#?}", program);
+    }
+    {
+        let source = SourceProgram::new(&db, "Hello\nWorld\nEveryOne".to_string());
+        let program = parse_source(&db, source);
+        println!("Source: {:#?}", source);
+        println!("Program: {:#?}", program);
+    }
 }
